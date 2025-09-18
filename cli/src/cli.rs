258fb33ef0f17cc8bde82c73b9aa62bf6a28726c
Rynco Maekawa <i@rynco.me>
2025-09-18 21:33:34 +0800

    feat: Add cli

---
+++ cli/src/cli.rs
@@ -0,0 +1,39 @@
+use std::path::PathBuf;
+
+#[derive(Debug, clap::Parser)]
+#[clap(name = "n2o4", version, author)]
+pub struct Args {
+    #[command(subcommand)]
+    pub subcommand: Subcommand,
+}
+
+#[derive(Debug, clap::Subcommand)]
+pub enum Subcommand {
+    Ninja(Box<NinjaSubcommand>),
+}
+
+#[derive(Debug, clap::Parser)]
+pub struct NinjaSubcommand {
+    /// The targets to build
+    pub targets: Vec<String>,
+
+    /// Change to DIR before doing anything else
+    #[clap(short = 'C', name = "DIR")]
+    pub chdir: Option<PathBuf>,
+
+    /// Show all command lines while building
+    #[clap(short, long)]
+    pub verbose: bool,
+
+    /// Don't show progress status, just command output
+    #[clap(long)]
+    pub quiet: bool,
+
+    /// Run N jobs in parallel (N > 0; default: number of CPU cores)
+    #[clap(short, long, name = "N")]
+    pub jobs: Option<usize>,
+
+    /// Dry run (don't commands but act like they succeeded)
+    #[clap(short = 'n', long)]
+    pub dry_run: bool,
+}
--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,22 @@
+use clap::Parser;
+
+use crate::cli::{Args, NinjaSubcommand};
+
+mod cli;
+
 fn main() {
-    println!("Hello, world!");
+    let argv0 = std::env::args().next();
+    if let Some(v) = argv0
+        && v.starts_with("ninja")
+    {
+        let argv = NinjaSubcommand::parse();
+        run_ninja(&argv);
+    } else {
+        let argv = Args::parse();
+        match argv.subcommand {
+            cli::Subcommand::Ninja(ninja_subcommand) => run_ninja(&ninja_subcommand),
+        }
+    }
 }
+
+fn run_ninja(cmd: &NinjaSubcommand) {}
