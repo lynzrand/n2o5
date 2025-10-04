dd794ea372b6bf309b002aa4f2b02ec8b86a7b99
Rynco Maekawa <i@rynco.me>
2025-10-04 11:04:44 +0800

    feat: Add useful re-exports

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,11 @@ pub mod exec;
 pub mod graph;
 pub mod shape;
 pub mod world;
+
+// Re-exports for convenience
+pub use db::ExecDb;
+pub use db::in_memory::InMemoryDb;
+pub use db::redb::ExecRedb;
+pub use exec::{ExecConfig, Executor};
+pub use graph::{BuildGraph, BuildId, FileId, GraphBuilder};
+pub use world::{LocalWorld, World};
